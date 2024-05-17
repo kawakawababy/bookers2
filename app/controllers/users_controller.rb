fac67ded51bf96b7095946a5a6973cd513efe2fc
kawakawababy <163483969+kawakawababy@users.noreply.github.com>
2024-05-17 02:18:47 +0000

    [Add] 投稿機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def show
-    #@user = User.find(params[:id])
-    #@books = @user.books
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+  end
+  
+  def update
   end
 end
